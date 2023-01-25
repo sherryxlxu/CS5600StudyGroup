cfc3b463862f0134a84f05bf85e24cadd78eb575
sherryxlxu <81453566+sherryxlxu@users.noreply.github.com>
2023-01-25 01:16:32 -0800

    qsize updated

--- queue.c
+++ queue.c
@@ -26,9 +26,7 @@ int size;
 
 void enqueue(queue_t* queue, int element) {
     
-	queue_t *cur = NULL;
-    // cur = (queue_t *) malloc(sizeof(queue_t));
-    cur = queue;
+	queue_t *cur = queue;
 
     while (cur->next != NULL) {
     	cur = cur->next;
@@ -78,22 +76,30 @@ void printQueue(queue_t* queue) {
 	printf("\n");
 }
 
+int qsize(queue_t * queue){
+	return size;
+}
+
 int main(int argc, char const *argv[])
 {
 	queue_t *queue = createQueue();
-	printQueue(queue);
+	// printQueue(queue);
+	printf("%d\n", qsize(queue));
 
 	enqueue(queue, 0);
-	printQueue(queue);
+	// printQueue(queue);
+	printf("%d\n", qsize(queue));
 
 	for (int i=1; i < 10; i++) {
 		int element = i;
 		enqueue(queue, element);
 	}
 	printQueue(queue);
+	printf("%d\n", qsize(queue));
 	for (int i = 1; i < 10; i++) {
 		dequeue(queue);
 	}
 	printQueue(queue);
+	printf("%d\n", qsize(queue));
 	return 0;
 }
